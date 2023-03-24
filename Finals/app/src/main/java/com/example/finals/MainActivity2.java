c1f8c56d6c19caf92585e21cdc08dd685b32bf02
BroTakuGH <93759335+BroTakuGH@users.noreply.github.com>
2023-03-24 13:49:42 +0800

    Fixed github

---
+++ Finals/app/src/main/java/com/example/finals/MainActivity2.java
@@ -0,0 +1,35 @@
+package com.example.finals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main2);
+
+        TextView signup = findViewById(R.id.login);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+
+                Intent intent=new Intent(MainActivity2.this,MainActivity3.class);
+                startActivity(intent);
+
+            }
+
+        });
+    }
+}
---
+++ Finals/app/src/main/java/com/example/finals/MainActivity3.java
@@ -0,0 +1,35 @@
+package com.example.finals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main3);
+
+        TextView signup = findViewById(R.id.login);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+
+                Intent intent=new Intent(MainActivity3.this,MainActivity2.class);
+                startActivity(intent);
+
+            }
+
+        });
+    }
+}
