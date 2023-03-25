a64a57a1f3672aeb28d371ca53d15ae962366b48
TheGentleDog <93631326+TheGentleDog@users.noreply.github.com>
2023-03-25 13:01:03 +0800

    Test
    Test

--- Finals/app/src/main/java/com/example/finals/MainActivity3.java
+++ Finals/app/src/main/java/com/example/finals/MainActivity3.java
@@ -7,23 +7,49 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
-
+private EditText id_input;
+private EditText email_input;
+private EditText name_input;
+private EditText password_input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
+
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main3);
+
+        id_input = findViewById(R.id.school_id_input);
+        email_input= findViewById(R.id.school_email_input);
+        name_input = findViewById(R.id.name_input);
+        password_input = findViewById(R.id.password_input);
+        Methods methods = new Methods();
 
         TextView signup = findViewById(R.id.login);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
+                methods.SetSchoolId(id_input.getText().toString());
+                methods.SetSchoolGmail(email_input.getText().toString());
+                methods.SetUserName(name_input.getText().toString());
+                methods.SetUserName(password_input.getText().toString());
+
+                if(methods.GetSchoolID().isEmpty()|| methods.GetPassword().isEmpty()){
+                    Toast.makeText(MainActivity3.this,"Please Enter ID or Password", Toast.LENGTH_SHORT).show();
+                }else{
+
+
+                }
+
+
 
                 Intent intent=new Intent(MainActivity3.this,MainActivity2.class);
                 startActivity(intent);
---
+++ Finals/app/src/main/java/com/example/finals/Methods.java
@@ -0,0 +1,43 @@
+package com.example.finals;
+
+public class Methods {
+    private String schoolID;
+    private String schoolGmail;
+    private String userName;
+    private String password;
+
+    public void SetSchoolId(String pid){
+    this.schoolID = pid;
+    }
+
+    public String GetSchoolID(){
+        return schoolID;
+    }
+
+    public void SetSchoolGmail(String pgmail){
+        this.schoolGmail = pgmail;
+    }
+
+    public String GetSchoolGmail(){
+        return schoolGmail;
+    }
+
+    public void SetUserName(String pname){
+        this.userName = pname;
+    }
+
+    public String GetName(){
+        return userName;
+    }
+    public void SetPass(String ppass){
+        this.password = ppass;
+    }
+
+    public String GetPassword(){
+        return password;
+    }
+
+
+
+
+}
