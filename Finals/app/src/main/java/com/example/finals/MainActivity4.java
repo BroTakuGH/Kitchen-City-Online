f274be056af7118ae8bcb24303c6e345515823c2
BroTakuGH <93759335+BroTakuGH@users.noreply.github.com>
2023-04-24 21:45:17 +0800

    REMOVED UNECESSARY SHITS

--- Finals/app/src/main/java/com/example/finals/MainActivity4.java
+++ Finals/app/src/main/java/com/example/finals/MainActivity4.java
@@ -21,28 +21,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.example.finals.databinding.ActivityMain4Binding;
-import com.google.android.gms.auth.api.signin.internal.Storage;
-import com.google.android.gms.tasks.OnFailureListener;
+
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+
 
 
 public class MainActivity4 extends AppCompatActivity {
 
-//    ActivityMain4Binding binding;
-//    ActivityResultLauncher<String> mTakePhoto;
-//    StorageReference storageReference;
-//    ProgressDialog progressDialog;
+
  Uri imageUri;
  private ImageView uploadImage;
  Button button;
@@ -120,78 +113,4 @@ public class MainActivity4 extends AppCompatActivity {
         MimeTypeMap mime = MimeTypeMap.getSingleton();
         return mime.getExtensionFromMimeType(contentResolver.getType(fileUri));
     }
-//        binding = ActivityMain4Binding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//
-//        mTakePhoto = registerForActivityResult(
-//                new ActivityResultContracts.GetContent(),
-//                new ActivityResultCallback<Uri>() {
-//                    @Override
-//                    public void onActivityResult(Uri result) {
-//
-//                        binding.imageView.setImageURI(result);
-//
-//                    }
-//                }
-//                );
-//        binding.button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                mTakePhoto.launch("image/*");
-//            }
-//        });
-//        binding.button2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                uploadImage();
-//
-//            }
-//        });
-//    }
-//
-//    private void uploadImage() {
-//
-//        progressDialog = new ProgressDialog(this);
-//        progressDialog.setTitle("Uploading File...");
-//        progressDialog.show();
-//
-//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.JAPAN);
-//        Date now = new Date();
-//        String fileName = formatter.format(now);
-//        storageReference = FirebaseStorage.getInstance().getReference("images/" + fileName);
-//
-//        storageReference.putFile(imageUri)
-//                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-//                    @Override
-//                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-//
-//                        binding.imageView.setImageURI(null);
-//                        Toast.makeText(MainActivity4.this, "Succefully Uploaded",Toast.LENGTH_SHORT).show();
-//                        if (progressDialog.isShowing()) {
-//                            progressDialog.dismiss();
-//                        }
-//                    }
-//                }).addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        if (progressDialog.isShowing()) {
-//                            progressDialog.dismiss();
-//                        }
-//                        Toast.makeText(MainActivity4.this, "Failed to Upload",Toast.LENGTH_SHORT).show();
-//
-//                    }
-//                });
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == 100 && data !=null && data.getData() != null){
-//            imageUri = data.getData();
-//            binding.imageView.setImageURI(imageUri);
-//        }
-//    }
 }
