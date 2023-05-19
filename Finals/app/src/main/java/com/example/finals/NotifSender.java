e561b009d66889206583ff6a3fb86d3373cef1b3
BroTakuGH <93759335+BroTakuGH@users.noreply.github.com>
2023-05-19 20:32:24 +0800

    Notif done test

---
+++ Finals/app/src/main/java/com/example/finals/NotifSender.java
@@ -0,0 +1,39 @@
+package com.example.finals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class NotifSender extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notif_sender);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
+
+        EditText title = findViewById(R.id.titleID);
+        EditText message = findViewById(R.id.messageId);
+        EditText token = findViewById(R.id.messageId);
+
+        findViewById(R.id.alldeviceId).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!title.getText().toString().isEmpty() && !message.getText().toString().isEmpty()){
+                FcmNotificationsSender notificationsSender = new FcmNotificationsSender("/topics/all",title.getText().toString(),
+                        message.getText().toString(), getApplicationContext(), NotifSender.this);
+                        notificationsSender.SendNotifications();
+                }
+                else {
+                    Toast.makeText(NotifSender.this, "Text cannot be blank!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
